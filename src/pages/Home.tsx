221616d455b09214b8e103708155e3e53a625770
pendkarsaimanish <pendkarsaimanish@outlook.com>
2025-08-07 17:00:21 +0530

    basic setup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+import React, { JSX, useState } from "react";
+
+export default function Home(): JSX.Element {
+  return <div>Home</div>;
+}
---
+++ src/pages/Register.tsx
@@ -0,0 +1,5 @@
+import React, { JSX } from "react";
+
+export default function Register(): JSX.Element {
+  return <div>Register</div>;
+}
